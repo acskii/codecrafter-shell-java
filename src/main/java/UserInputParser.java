50c822e7986c73a841a952dc9b6a8340cd0ab738
acskii <andrew.sameh.06@gmail.com>
2025-06-24 17:56:05 +0300

    refactor: rewrote how user input is interpreted to support backslashes and quotes + fix bugs

--- src/main/java/UserInputParser.java
+++ src/main/java/UserInputParser.java
@@ -17,32 +17,103 @@ public class UserInputParser {
         boolean isInsideSingleQuote = false;
         boolean isInsideDoubleQuote = false;
         boolean passedBackslash = false;
-        int i = 0;
+        int i = 0, n = input.length();
 
         while (i < input.length() && !Character.isWhitespace(input.charAt(i))) i++;
         while (i < input.length() && Character.isWhitespace(input.charAt(i))) i++;
 
-        // TODO: Backslash inside single quote + outside quotes
-        for (int j = i; j < input.length(); j++) {
-            char c = input.charAt(j);
+//        // TODO: Backslash inside single quote + outside quotes
+//        for (int j = i; j < input.length(); j++) {
+//            char c = input.charAt(j);
+//
+//            System.out.print(c + " :  ");
+//            System.out.print(isInsideSingleQuote);
+//            System.out.print(isInsideDoubleQuote);
+//            System.out.print(passedBackslash);
+//            System.out.println();
+//
+//            if (c == '\\') {
+//                if (isInsideSingleQuote) {
+//                    builder.append(c);
+//                    continue;
+//                }
+//
+//                if (passedBackslash) {
+//                    builder.append(c);
+//                    passedBackslash = false;
+//                    continue;
+//                }
+//
+//                passedBackslash = true;
+//            } else if (passedBackslash) {
+//                builder.append(c);
+//                passedBackslash = false;
+//            } else if (c == '\'') {
+//                isInsideSingleQuote = !isInsideSingleQuote;
+//                if (isInsideDoubleQuote) builder.append(c);
+//            } else if (c == '"') {
+//                isInsideDoubleQuote = !isInsideDoubleQuote;
+//            } else if (isInsideDoubleQuote || isInsideSingleQuote || c != ' ') {
+//                builder.append(c);
+//            } else if (!builder.isEmpty()) {
+//                result.add(builder.toString());
+//                builder.delete(0, builder.length());
+//            }
+//        }
+//
+//        if (!builder.isEmpty()) result.add(builder.toString());
+//
+//        return result.toArray(String[]::new);
 
-            if (c == '\\' && !isInsideSingleQuote && !isInsideDoubleQuote) {
-                passedBackslash = true;
-            } else if (passedBackslash) {
-                passedBackslash = false;
+        while (i < n) {
+            char c = input.charAt(i);
+
+            if (isInsideSingleQuote) {
+                if (c == '\'') {
+                    isInsideSingleQuote = false;
+                } else {
+                    builder.append(c);
+                }
+                i++;
+                continue;
+            }
+
+            if (isInsideDoubleQuote) {
+                if (c == '"') {
+                    isInsideDoubleQuote = false;
+                    i++;
+                    continue;
+                }
+                if (c == '\\' && i+1 < n && "\"\\$`".indexOf(input.charAt(i+1)) >= 0) {
+                    builder.append(input.charAt(i+1));
+                    i += 2;
+                    continue;
+                }
                 builder.append(c);
-            } else if (c == '"' && !isInsideSingleQuote) {
-                isInsideDoubleQuote = !isInsideDoubleQuote;
-            } else if (c == '\'' && !isInsideDoubleQuote) {
-                isInsideSingleQuote = !isInsideSingleQuote;
-            } else if (isInsideDoubleQuote | isInsideSingleQuote | c != ' ') {
+                i++;
+                continue;
+            }
+
+            if (c == '\'') {
+                isInsideSingleQuote = true;
+                i++;
+            } else if (c == '"') {
+                isInsideDoubleQuote = true;
+                i++;
+            } else if (c == '\\' && i+1 < n) {
+                builder.append(input.charAt(i+1));
+                i += 2;
+            } else if (Character.isWhitespace(c)) {
+                if (!builder.isEmpty()) {
+                    result.add(builder.toString());
+                    builder.setLength(0);
+                }
+                while (i < n && Character.isWhitespace(input.charAt(i))) i++;
+            } else {
                 builder.append(c);
-            } else if (!builder.isEmpty()) {
-                result.add(builder.toString());
-                builder.delete(0, builder.length());
+                i++;
             }
         }
-
         if (!builder.isEmpty()) result.add(builder.toString());
 
         return result.toArray(String[]::new);
