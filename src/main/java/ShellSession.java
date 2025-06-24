7a12ddc84c19254f11431e825d1a88b5aee08398
acskii <andrew.sameh.06@gmail.com>
2025-06-24 20:07:45 +0300

    fix: won't break if HOME env variable not set

--- src/main/java/ShellSession.java
+++ src/main/java/ShellSession.java
@@ -1,8 +1,16 @@
 import java.io.File;
 
 public class ShellSession {
-    private static final File homeDirectory = new File(System.getenv("HOME"));
+    private static final File homeDirectory;
     private static File currentWorkingDirectory = new File(System.getProperty("user.dir"));
+
+    static {
+        if (System.getenv("HOME") == null) {
+            homeDirectory = currentWorkingDirectory;
+        } else {
+            homeDirectory = new File(System.getenv("HOME"));
+        }
+    }
 
     public static File getWD() {
         return currentWorkingDirectory;
