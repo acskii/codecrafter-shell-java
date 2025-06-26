140d5f3b859cc5d6b9fe923247dbe230443f5548
acskii <andrew.sameh.06@gmail.com>
2025-06-26 22:34:33 +0300

    feature: up key history

--- src/main/java/CommandHistory.java
+++ src/main/java/CommandHistory.java
@@ -4,10 +4,11 @@ import java.util.Stack;
 
 public class CommandHistory {
     private static final List<String> history = new ArrayList<>();
-
+    private static final Stack<String> upHistory = new Stack<>();
 
     public static void addCommand(String command) {
         history.add(command);
+        upHistory.push(command);
     }
 
     public static String[] getPreviousCommands() {
@@ -16,5 +17,9 @@ public class CommandHistory {
 
     public static String[] getPreviousCommands(int n) {
         return history.subList(history.size() - n, history.size()).toArray(String[]::new);
+    }
+
+    public static String getLatestCommand() {
+        return upHistory.pop();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,163 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    private static final Scanner reader = new Scanner(System.in);
+//    private static final Scanner reader = new Scanner(System.in);
+    private static Process rawMode;
 
     public static void main(String[] args) {
-
         CommandInvoker invoker = new CommandInvoker();
-
         BuiltinCommands.registerAll();  // register all builtin commands
 
-        while (true) {
-            System.out.print("$ ");
-            String input = reader.nextLine();
-            String command = UserInputParser.getCommand(input);
-            String[] arguments = UserInputParser.getArgs(input);
-            String stream = UserInputParser.getOutputStream(input);
+        enableRawMode();
 
-            CommandHistory.addCommand(input);
-            Logger.setAppend(UserInputParser.isAppendRedirector(input));
+        try {
+            while (true) {
+                String input = read();
+                String command = UserInputParser.getCommand(input);
+                String[] arguments = UserInputParser.getArgs(input);
+                String stream = UserInputParser.getOutputStream(input);
 
-            if (UserInputParser.hasOutputRedirector(input)) {
-                Logger.set(stream, null);
-            } else if (UserInputParser.hasErrRedirector(input)) {
-                Logger.set(null, stream);
-            }
+                CommandHistory.addCommand(input);
+                Logger.setAppend(UserInputParser.isAppendRedirector(input));
 
-            try {
-                Command cmd = CommandRegistry.create(command, arguments);
-                invoker.setCommand(cmd);
-                invoker.invoke();
-            } catch (IllegalArgumentException e) {
-                String executable = Executable.findExecutable(command);
+                if (UserInputParser.hasOutputRedirector(input)) {
+                    Logger.set(stream, null);
+                } else if (UserInputParser.hasErrRedirector(input)) {
+                    Logger.set(null, stream);
+                }
 
-                if (executable != null) {
-                    invoker.setCommand(new ExecutableCommand(command, arguments));
+                try {
+                    Command cmd = CommandRegistry.create(command, arguments);
+                    invoker.setCommand(cmd);
                     invoker.invoke();
-                } else {
-                    Logger.err(String.format("%s: command not found", command));
+                } catch (IllegalArgumentException e) {
+                    String executable = Executable.findExecutable(command);
+
+                    if (executable != null) {
+                        invoker.setCommand(new ExecutableCommand(command, arguments));
+                        invoker.invoke();
+                    } else {
+                        Logger.err(String.format("%s: command not found", command));
+                    }
+                }
+
+                Logger.set(null, null);
+            }
+        } finally {
+            disableRawMode();
+        }
+    }
+
+    private static void enableRawMode() {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh", "-c", "stty -echo -icanon min 1 < /dev/tty");
+            processBuilder.directory(new File("").getCanonicalFile());
+            rawMode = processBuilder.start();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    new ProcessBuilder("/bin/sh", "-c", "stty sane < /dev/tty").start().waitFor();
+                } catch (Exception e) {
+                    Logger.err("Failed to reset terminal: " + e);
+                }
+            }));
+        } catch (IOException e) {
+            Logger.err(e.toString());
+        }
+    }
+
+    private static void disableRawMode() {
+        try {
+            if (rawMode != null && rawMode.isAlive()) {
+                new ProcessBuilder("/bin/sh", "-c", "stty sane < /dev/tty")
+                        .inheritIO()
+                        .start()
+                        .waitFor();
+                rawMode.destroy();
+            }
+        } catch (Exception e) {
+            Logger.err(e.toString());
+        }
+    }
+
+    private static void clearLine(int spaces) {
+        System.out.print('\r');
+        for (int i = 0; i < spaces; i++) System.out.print(" ");
+        System.out.print('\r');
+    }
+
+    private static String read() {
+        try {
+            System.out.print("$ ");
+            StringBuilder buffer = new StringBuilder();
+
+            while (true) {
+                int input = System.in.read();
+
+                if (input == -1) {
+                    return "";
+                }
+                char character = (char) input;
+
+                switch (character) {
+                    case '\r':
+                        break;
+
+                    case '\n': {
+                        System.out.print('\n');
+                        return buffer.toString();
+                    }
+
+                    case 0x7f: {
+                        // Backspace
+                        if (buffer.isEmpty()) continue;
+                        buffer.deleteCharAt(buffer.length());
+                        System.out.print("\b \b");
+                        break;
+                    }
+
+                    // UP key is ESC [ A
+                    // DOWN key is ESC [ B
+
+                    case 0x1b: {
+                        // ESC
+                        int bracket = System.in.read();
+                        int code = System.in.read();
+
+                        if (bracket == 91) {
+                            // [
+                            if (code == 65) {
+                                // A -> UP KEY
+                                String previous = CommandHistory.getLatestCommand();
+                                clearLine(2 + buffer.length());
+                                System.out.print("$ ");
+                                System.out.print(previous);
+
+                                buffer.delete(0, buffer.length());
+                                buffer.append(previous);
+                                break;
+                            } else if (code == 66) {
+                                // B -> DOWN KEY
+
+                                break;
+                            }
+                        }
+                        break;
+                    }
+
+                    default: {
+                        buffer.append(character);
+                        System.out.print(character);
+                        break;
+                    }
                 }
             }
-
-            Logger.set(null, null);
+        } catch (IOException e) {
+            Logger.err(e.toString());
         }
+
+        return "";
     }
 }
