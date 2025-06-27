72bef69a2cea9462424bfa23fb2d94c49d678e9c
acskii <andrew.sameh.06@gmail.com>
2025-06-27 22:05:31 +0300

    chore: separated PATH env to the ShellSession

--- src/main/java/ShellSession.java
+++ src/main/java/ShellSession.java
@@ -2,6 +2,7 @@ import java.io.File;
 
 public class ShellSession {
     private static final File homeDirectory;
+    private static final File pathDirectory;
     private static File currentWorkingDirectory = new File(System.getProperty("user.dir"));
 
     static {
@@ -10,10 +11,20 @@ public class ShellSession {
         } else {
             homeDirectory = new File(System.getenv("HOME"));
         }
+
+        if (System.getenv("PATH") == null) {
+            pathDirectory = currentWorkingDirectory;
+        } else {
+            pathDirectory = new File(System.getenv("PATH"));
+        }
     }
 
     public static File getWD() {
         return currentWorkingDirectory;
+    }
+
+    public static File getPath() {
+        return pathDirectory;
     }
 
     public static File getHome() {
